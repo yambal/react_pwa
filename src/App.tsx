4cb40007982bbe8747d64ad6568eee051b11efa6
June YAMAMOTO <june@yambal.net>
2020-06-09 19:51:09 +0900

    v2

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React v2
         </a>
       </header>
     </div>
