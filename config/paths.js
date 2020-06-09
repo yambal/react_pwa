caa3d697d2356507fc8ecc0bc860eca118007ccb
June YAMAMOTO <june@yambal.net>
2020-06-09 18:37:51 +0900

    PWA

--- config/paths.js
+++ config/paths.js
@@ -17,7 +17,7 @@ const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 // like /todos/42/static/js/bundle.7289d.js. We have to know the root.
 const publicUrlOrPath = getPublicUrlOrPath(
   process.env.NODE_ENV === 'development',
-  './',
+  require(resolveApp('package.json')).homepage,
   process.env.PUBLIC_URL
 );
 
@@ -52,7 +52,7 @@ const resolveModule = (resolveFn, filePath) => {
 module.exports = {
   dotenv: resolveApp('.env'),
   appPath: resolveApp('.'),
-  appBuild: resolveApp('docs'),
+  appBuild: resolveApp('build'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
